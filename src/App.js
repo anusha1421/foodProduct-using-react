b9c9516e993aae997105ec1bfb3ace58cfeead69
anusha1421 <117527304+anusha1421@users.noreply.github.com>
2024-09-20 03:16:08 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductDetail from "./Components/ProductDetail"; // Make sure it's ProductDetailPage.js
+import './styles/styles.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Route for the homepage */}
+          <Route path="/" element={<HomePage />} />
+          
+          {/* Route for the product detail page */}
+          <Route path="/product/:barcode" element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
